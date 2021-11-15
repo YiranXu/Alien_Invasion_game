602238e339344d3c627a1a519515ac61cb2a431c
YiranXu <yx1350@nyu.edu>
2021-11-15 16:37:04 -0500

    all

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings():
+    """ A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        """Initialize the game's setting"""
+        #Screen settings
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
+        #self.bg_color=(0,0,250)
+
+        #ship settings
+        self.ship_speed_factor=1.5
+    
+        
