7f188f00e15988ff0792e10d49acf1ba5d66c529
YiranXu <yx1350@nyu.edu>
2021-11-22 16:14:09 -0500

    collide

--- game_functions.py
+++ game_functions.py
@@ -51,7 +51,7 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     #make the most recently drawn screen visible
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens,bullets):
     """Update position of bullets and get rid of old bullets"""
     #Update bullet positions
     bullets.update()
@@ -60,6 +60,9 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom<=0:
             bullets.remove(bullet)
+    #Check for any bullets that have hit aliens
+    #If so, get rid of the bullet and the alien
+    collisons=pygame.sprite.groupcollide(bullets,aliens,True,True)
 
 def fire_bullet(ai_settings,screen,ship,bullets):
     """Fire a bullet if limit not reached yet."""
