5c9f0e3bab6022236e6cb43fa4f30d1004f25607
YiranXu <yx1350@nyu.edu>
2021-11-29 23:01:39 -0500

    reset to bottom when ship hits

--- game_functions.py
+++ game_functions.py
@@ -31,7 +31,7 @@ def check_keyup_events(event,ship):
         ship.moving_up=False
     elif event.key==pygame.K_DOWN:
         ship.moving_down=False
-def check_events(ai_settings,screen,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -40,6 +40,15 @@ def check_events(ai_settings,screen,ship,bullets):
                 check_keydown_events(event,ai_settings,screen,ship,bullets)
             elif event.type==pygame.KEYUP:
                 check_keyup_events(event,ship)
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                mouse_x,mouse_y=pygame.mouse.get_pos()
+                check_play_button(stats,play_button,mouse_x,mouse_y)
+
+def check_play_button(stats,play_button,mouse_x,mouse_y):
+    """Start a new game when the player clicks Play."""
+    if play_button.rect.collidepoint(mouse_x,mouse_y):
+        stats.game_active=True
+
 def update_screen(ai_settings,screen,stats, ship,aliens,bullets,play_button):
     """Update images on the screen and flip to the new screen."""
     #redraw the screen during each pass through the loop
@@ -143,7 +152,7 @@ def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
         #Create a new fleet and center the ship
         create_fleet(ai_settings,screen,ship,aliens)
         ship.center_ship()
-
+        
         #Pause
         sleep(0.5)
     else:
@@ -155,6 +164,7 @@ def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
         if alien.rect.bottom>=screen_rect.bottom:
             #Treat this the same as if the ship got hit.
             ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+            
             break
 
 def update_alien(ai_settings,stats,screen,ship,aliens,bullets):
@@ -166,5 +176,6 @@ def update_alien(ai_settings,stats,screen,ship,aliens,bullets):
     #Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(ship,aliens):
         ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
+        
     #Look for aliens hitting the bottom of the screen
     check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@ class Settings():
         self.ship_speed_factor=1.5
         self.ship_limit=3
         
+        
         #Alien settings
         self.alien_speed_factor=1
         self.fleet_drop_speed=10
